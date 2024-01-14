f2d4148e2d494c0dad0cdb31374c641f19746fdf
Shubham Singh <shubh622005@gmail.com>
2024-01-15 00:10:37 +0530

    Add .gitignore, Cargo.lock, Cargo.toml, and src/main.rs files

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about , long_about=None)]
+#[command(propagate_version = true)]
+
+struct CLI {
+    #[command(subcommand)]
+    command: Arguments,
+}
+
+#[derive(Subcommand)]
+enum Arguments {
+    
+}
+
+fn main() {}
